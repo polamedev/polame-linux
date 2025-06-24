ab8a1f7f36e7745bf6e5b6c951884044ca48420a
nupolame <nupolame@gmail.com>
2025-06-24 22:25:19 +0300

    Добавить clang-format & скрипт единственной конфигурации

---
+++ configure-once.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Скрипт единственной конфигурации
+# Запуск: source ~/nupola-linux/configure-once.sh  
+
+# Настройка необходимых символьных ссылок в системе
+
+# Создание символьной ссылки на .clang-format
+ln -s ~/nupola-linux/.clang-format ~/.clang-format
