9f16a1eb0c105ae6205d20573505dbab1f36fc1e
nupolame <nupolame@gmail.com>
2025-06-25 13:11:13 +0300

    linkopt скрипт

---
+++ linkopt.sh
@@ -0,0 +1,18 @@
+# Скрипт для создания символьных ссылок на файл из текущей дирректории в /opt/bin
+# Пример ~/nupola-linux/linkopt.sh (-f) riscv-none-elf-gcc
+# -f - флаг для перезаписи ссылок
+force_flag="-f"
+
+if [ "$1" = $force_flag ]
+
+then
+fullpath=`realpath $2`
+filename=`basename $2`
+sudo ln -f -s $fullpath /opt/bin/$filename
+
+else
+fullpath=`realpath $1`
+filename=`basename $1`
+sudo ln -s $fullpath /opt/bin/$filename
+
+fi
